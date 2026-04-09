ef338f6afd1c6ae8bd7dbd9c97515ed664816c8d
GavinRobson <gtrobson3@gmail.com>
2026-04-09 17:22:27 -0400

    BIG CHANGE: React-Router-Dom | Still technically a Single Pgae Application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,17 @@
-import { useState, useEffect } from "react";
-import { getAllPosts } from "../api/posts";
-import { Create } from "./components/Form";
+import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/navigation/NavBar";
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    async function loadPosts() {
-      const result = await getAllPosts();
-      if (result.error) {
-        console.error(result.error);
-        return;
-      }
-
-      setPosts(result.data);
-    }
-
-    loadPosts();
-  }, []);
-
   return (
     <div className="min-h-screen w-screen">
       <NavBar />
-      <div style={{ paddingLeft: "200px", paddingRight: "200px", fontFamily: "sans-serif" }}>
-        <h1>Compile Discussion Board</h1>
-
-        <Create 
-          setPosts={setPosts}
-        />
-
-        {posts.length === 0 ? (
-          <p>Loading posts...</p>
-        ) : (
-          posts?.map((post) => (
-            <div
-              key={post._id}
-              style={{
-                border: "1px solid #ccc",
-                margin: "10px 0",
-                padding: "15px",
-                borderRadius: "5px",
-              }}
-            >
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-              <small>Posted by {post.author}</small>
-              <br />
-              <small>Tags: {post.tags.join(", ")}</small>
-            </div>
-          ))
-        )}
-      </div>
-    </div>  
+      <Routes>
+        <Route path="/" element={ <HomePage/> } />
+        <Route path="/test" element={ <LoginPage /> } />
+      </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,53 @@
+import { Create } from "../components/Form"
+import { useState, useEffect } from "react"
+import { getAllPosts } from "../../api/posts";
+
+export default function HomePage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      const result = await getAllPosts();
+      if (result.error) {
+        console.error(result.error);
+        return;
+      }
+
+      setPosts(result.data);
+    }
+
+    loadPosts();
+  }, []);
+
+  return (
+    <>
+      <h1>Compile Discussion Board</h1>
+
+      <Create
+        setPosts={setPosts}
+      />
+
+      {posts.length === 0 ? (
+        <p>Loading posts...</p>
+      ) : (
+        posts?.map((post) => (
+          <div
+            key={post._id}
+            style={{
+              border: "1px solid #ccc",
+              margin: "10px 0",
+              padding: "15px",
+              borderRadius: "5px",
+            }}
+          >
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <small>Posted by {post.author}</small>
+            <br />
+            <small>Tags: {post.tags.join(", ")}</small>
+          </div>
+        ))
+      )}
+    </>
+  )
+}
