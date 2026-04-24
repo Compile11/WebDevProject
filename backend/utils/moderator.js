b1929571fe3f80da6ebbaa1f5bbd3a6197c6fe50
Noah446F6E6E656C6C79 <ndonnel1@students.towson.edu>
2026-04-23 20:57:48 -0400

    fixed automod finally

--- backend/utils/moderator.js
+++ backend/utils/moderator.js
@@ -5,9 +5,10 @@ const getToxicityScore = async (text) => {
         const API_KEY = process.env.PERSPECTIVE_API_KEY;
         const URL = `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${API_KEY}`;
 
-        const response = await axios.get(URL, {
-            comment: {text},
-            language: ["en"],
+        // Change from axios.get to axios.post
+        const response = await axios.post(URL, {
+            comment: {text: text},
+            languages: ["en"], // Best practice: use 'languages' plural
             requestedAttributes: {
                 TOXICITY: {},
                 IDENTITY_ATTACK: {},
@@ -15,14 +16,15 @@ const getToxicityScore = async (text) => {
             }
         });
 
-        //return highest score amongst attributes
         const attr = response.data.attributeScores;
+
         return {
             toxicity: attr.TOXICITY.summaryScore.value,
             identity: attr.IDENTITY_ATTACK.summaryScore.value,
             insult: attr.INSULT.summaryScore.value
         };
-    }catch(err) {
+
+    } catch(err) {
         console.error("MODERATION API ERROR", err.message);
     }
 };
