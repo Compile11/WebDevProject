8f3882fed6c303b95b9647292663fc3a3e973313
GavinRobson <gtrobson3@gmail.com>
2026-05-05 01:21:50 -0400

    Updated start.sh script

--- start.sh
+++ start.sh
@@ -10,9 +10,16 @@ cd ../frontend
 npm run dev &
 FRONTEND_PID=$!
 
-stripe listen --forward-to localhost:5000/api/stripe/webhook > /dev/null &
-STRIPE_PID=$!
+if command -v stripe >/dev/null 2>&1; then
+  echo "Stripe CLI found. Startin webhook listener..."
+  stripe listen --forward-to localhost:5000/api/stripe/webhook > /dev/null &
+  STRIPE_PID=$!
+else
+  echo "Stripe CLI not found. Skipping webhook listener.."
+fi
 
 trap "kill $BACKEND_PID $FRONTEND_PID" EXIT
 
 wait
+
+
